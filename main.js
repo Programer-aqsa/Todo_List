3ae6672975d3cb7768eed929d7341a0b8dbd27bc
Aqsa Shahzad <160316789+Programer-aqsa@users.noreply.github.com>
2024-04-02 12:32:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let answers = await inquirer.prompt([
+        {
+            name: "todo",
+            message: "what you want to add in your Todos?",
+            type: "input",
+        },
+        {
+            name: "addMore",
+            message: "Do you want to add more?",
+            type: "confirm",
+            default: "false"
+        },
+    ]);
+    todos.push(answers.todo);
+    condition = answers.addMore;
+    console.log(todos);
+}
